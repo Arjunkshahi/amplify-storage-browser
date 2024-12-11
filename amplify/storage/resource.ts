d0fffe5c7294a5d7bd12be1c8d4ebedd191598f7
Arjunkshahi <135677689+Arjunkshahi@users.noreply.github.com>
2024-12-10 19:09:25 -0500

    Removed guest functionality and chaged path names

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,9 +3,8 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'myProjectFiles',
   access: (allow) => ({
-    'media/profile-pictures/{entity_id}/*': [
+    'media/photos/{entity_id}/*': [
       allow.entity('identity').to(['read', 'write', 'delete']),
-      allow.guest.to(['read']),
       allow.authenticated.to(['read'])
     ]
   })
